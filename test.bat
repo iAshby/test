e207d0ade41a61562fa4132807744ba4ae130ab5
tomthumby <122403485+tomthumby@users.noreply.github.com>
2023-07-07 11:19:35 -0600

    Add files via upload

---
+++ test.bat
@@ -0,0 +1,4 @@
+mkdir %USERPROFILE%\Desktop\Outlook_test
+echo "test test test test test" > %USERPROFILE%\Desktop\Outlook_test\test_worked.txt
+echo "Safe Attachment pwnage" >> %USERPROFILE%\Desktop\Outlook_test\test_worked.txt
+calc.exe
